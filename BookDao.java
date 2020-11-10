30179bc2fa4cc877084c98f52b6287120b5c9d0c
JyothiPriyaKondepati <69138976+JyothiPriyaKondepati@users.noreply.github.com>
2020-11-10 17:18:37 +0530

    Add files via upload

---
+++ BookDao.java
@@ -0,0 +1,10 @@
+package com.demo;
+import java.util.List;
+
+public interface BookDao {
+	List<Book> getAllBooks();
+	Book updateBook( int id,int page );
+	Book deleteBook( int id );
+    void addBook(Book book );
+
+}
